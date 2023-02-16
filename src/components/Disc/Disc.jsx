1baa8a9695ede8aed3a96cc073dbaccdd9843e07
Thomas Hodges <thodges@javascriptonline.org>
2023-02-16 14:35:10 -0800

    switching to solid - some cleanup needed

--- src/components/Disc/Disc.jsx
+++ src/components/Disc/Disc.jsx
@@ -1,8 +1,10 @@
-import { Fragment, useEffect, useMemo } from "react";
+import { Fragment, useEffect, useMemo, useState } from "react";
 import { useControls } from "leva";
-import { DoubleSide, MeshBasicMaterial } from "three";
+import { DoubleSide, MeshBasicMaterial, Vector2 } from "three";
 import Line from "./Line";
 import debounce from "../../utils/debounce";
+import RotationObject from "../RotationObject";
+import CurveyLine from "../RotationObject/CurveyLine";
 
 const Disc = ({
   solid = {
@@ -27,13 +29,30 @@ const Disc = ({
   );
   const controls = useControls(options);
 
+  const [displaySolid, setDisplaySolid] = useState(false);
+
+  const points = useMemo(() => {
+    // const { domain, func, resolution } = solid;
+    const pts = [];
+    const numberPoints = resolution * (controls.x - domain[0]) + 1;
+    const dx = (controls.x - domain[0]) / (numberPoints - 1);
+    for (let i = 0; i < numberPoints; i++) {
+      const x = domain[0] + i * dx;
+      pts.push(new Vector2(func(x), x));
+    }
+    return pts;
+  }, [controls.x]);
+
   const debouncedChangeHandler = useMemo(
-    () => debounce(() => console.log("BOUNCE"), 500), // make solid and change to solid
+    () =>
+      debounce(() => {
+        setDisplaySolid(true);
+      }, 500),
     []
   );
 
   useEffect(() => {
-    console.log("move"); //change to discs
+    setDisplaySolid(false);
     debouncedChangeHandler();
   }, [controls.x]);
 
@@ -57,7 +76,52 @@ const Disc = ({
     return discsArray;
   }, [controls.x]);
 
-  return <>{discs}</>;
+  console.log(domain[0], controls.x, domain[0] + step);
+  return (
+    <>
+      {!displaySolid && discs}
+      {displaySolid && controls.x > domain[0] && (
+        <RotationObject
+          solid={{
+            domain: [domain[0], controls.x],
+            func: func,
+            resolution: resolution,
+          }}
+          sides={sides}
+          normalMaterial={false}
+        />
+      )}
+      {displaySolid && (
+        <>
+          <mesh rotation-y={-Math.PI / 2} position-x={domain[0]}>
+            <meshBasicMaterial
+              attach="material"
+              color="#5a5a5a"
+              side={DoubleSide}
+            />
+            <circleGeometry args={[func(domain[0]), sides]} />
+          </mesh>
+          <mesh rotation-y={-Math.PI / 2} position-x={controls.x + step}>
+            <meshBasicMaterial
+              attach="material"
+              color="#5a5a5a"
+              side={DoubleSide}
+            />
+            <circleGeometry args={[func(controls.x + step), sides]} />
+          </mesh>
+          <Line
+            start={[domain[0], 0, 0]}
+            end={[domain[0], func(domain[0]), 0]}
+          />
+          <Line
+            start={[controls.x + step, 0, 0]}
+            end={[controls.x + step, func(controls.x + step), 0]}
+          />
+          <CurveyLine points={points} />
+        </>
+      )}
+    </>
+  );
 };
 
 export default Disc;
